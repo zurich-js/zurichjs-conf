92cfa526c236dfc4184f13f20163838e13a924d8
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2025-12-05 15:42:40 +0100

    Remove stripe sentance

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -755,13 +755,6 @@ export default function CartPage() {
                           />
                         </div>
 
-                        {/* Trust Badges */}
-                        <div className="flex items-center gap-3 text-sm text-brand-gray-light">
-                          <svg className="w-5 h-5 text-green-500 shrink-0" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                          </svg>
-                          <span>Secure payment via Stripe</span>
-                        </div>
                     </div>
                   </div>
                 </div>
