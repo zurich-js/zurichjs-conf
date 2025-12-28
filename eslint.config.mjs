135bcad78aacb0c2bf450bb6ce692443885d6751
colinscz <19342760+colinscz@users.noreply.github.com>
2025-12-28 23:18:44 +0100

    fix: address linter errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,6 @@
-import { fileURLToPath } from "url";
 import { defineConfig } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
-import path from "node:path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig([
     {
