c471b5dc0c3b1cbdd0fd0ec518f8fdfe72e8e6e8
Faris Aziz <53216647+farisaziz12@users.noreply.github.com>
2026-02-14 15:27:48 +0100

    update migration script

--- supabase/migrations/20260207102812_add_rls_policies.sql
+++ supabase/migrations/20260207102812_add_rls_policies.sql
@@ -1,6 +1,3 @@
-drop extension if exists "pg_net";
-drop extension if exists "pgjwt";
-
 revoke delete on table "public"."cfp_reviewers" from "anon";
 
 revoke insert on table "public"."cfp_reviewers" from "anon";
@@ -178,16 +175,4 @@ create policy "profiles_select_own"
   for select
   to authenticated
 using ((id = ( SELECT auth.uid() AS uid)));
-
-
-CREATE TRIGGER objects_delete_delete_prefix AFTER DELETE ON storage.objects FOR EACH ROW EXECUTE FUNCTION storage.delete_prefix_hierarchy_trigger();
-
-CREATE TRIGGER objects_insert_create_prefix BEFORE INSERT ON storage.objects FOR EACH ROW EXECUTE FUNCTION storage.objects_insert_prefix_trigger();
-
-CREATE TRIGGER objects_update_create_prefix BEFORE UPDATE ON storage.objects FOR EACH ROW WHEN (((new.name <> old.name) OR (new.bucket_id <> old.bucket_id))) EXECUTE FUNCTION storage.objects_update_prefix_trigger();
-
-CREATE TRIGGER prefixes_create_hierarchy BEFORE INSERT ON storage.prefixes FOR EACH ROW WHEN ((pg_trigger_depth() < 1)) EXECUTE FUNCTION storage.prefixes_insert_trigger();
-
-CREATE TRIGGER prefixes_delete_hierarchy AFTER DELETE ON storage.prefixes FOR EACH ROW EXECUTE FUNCTION storage.delete_prefix_hierarchy_trigger();
-
 
