240227710782ceb6ff21d64ae0b2c8106ac33286
Ilie Bogdan <ilie06@gmail.com>
2026-09-01 08:15:46 +0200

    Update NavBar.tsx

--- src/components/organisms/NavBar.tsx
+++ src/components/organisms/NavBar.tsx
@@ -12,12 +12,10 @@ export interface NavBarProps {
 }
 
 const navLinks = [
-  { label: "About", href: "/about" },
   { label: "Schedule", href: "/schedule" },
   { label: "Speakers", href: "/speakers" },
   { label: "Workshops", href: "/workshops" },
   { label: "Volunteer", href: "/volunteer" },
-  { label: "Sponsor us", href: "/sponsorship" },
 ];
 
 const socialLinks = [
